5eb4c2d017213eea7c534192e8bc3ab2a122f9e0
Walter Martín Lopes <wmlopes0@gmail.com>
2024-09-16 12:37:40 +0200

    Before implement IAccountController

--- accountsService/src/main/java/com/myOnlineShoppingService/accountsService/filters/IpFilter.java
+++ accountsService/src/main/java/com/myOnlineShoppingService/accountsService/filters/IpFilter.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Component
-@Profile("prod")
+@Profile("default")
 public class IpFilter implements Filter {
 
     private static final List<String> ALLOWED_IPS = Arrays.asList(
