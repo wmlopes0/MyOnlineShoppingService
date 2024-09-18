daf0148d6d8cafb3d94ead386621702f02ce75d5
Walter Martín Lopes <wmlopes0@gmail.com>
2024-09-18 09:13:35 +0200

    Updates

--- accountsService/src/test/java/com/myOnlineShoppingService/accountsService/services/AccountServiceImplTest.java
+++ accountsService/src/test/java/com/myOnlineShoppingService/accountsService/services/AccountServiceImplTest.java
@@ -8,11 +8,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class AccountServiceImplTest {
 
     @Test
-    @DisplayName("Prueba positiva de findByOwner_Id")
-    void findAccountByIdAndOwnerId() {
+    @DisplayName("Prueba positiva de findAccountByIdAndOwnerId")
+    void findAccountByIdAndOwnerId_PositiveTest() {
     }
 
     @Test
+    @DisplayName("Prueba negativa de findAccountByIdAndOwnerId")
+    void findAccountByIdAndOwnerId_NegativeTest() {
+    }
+
+    @Test
+    @DisplayName("Prueba positiva de findByOwner_Id")
     void createAccount() {
     }
 
